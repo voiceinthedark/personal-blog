29ce6c81646f5bfdf4e6fb7df7643c1ec837f01a
voiceinthedark <darkrisingforce@gmail.com>
2023-08-06 17:54:03 +0300

    Added dynamic indexing through contentLoader

--- articles.data.js
+++ articles.data.js
@@ -1,10 +1,16 @@
 import scanArticles from './src/utils/scanArticles.js'
+import { createContentLoader } from 'vitepress'
 
-export default {
-    load(){
-        let articles = [];
-        return{
-            data: scanArticles('./src/articles', articles)
-        }
-    }
-}
+// export default {
+//     load(){
+//         let articles = [];
+//         return{
+//             data: scanArticles('./src/articles', articles).map((article) => {
+//                 return `- [${article.split('/').pop().split('.md')[0]}](${article})\n`
+                
+//             })
+//         }
+//     }
+// }
+
+export default createContentLoader('./src/articles/2023/*.md')
--- src/articles/index.md
+++ src/articles/index.md
@@ -4,13 +4,9 @@
 
 # Recent Articles
 
-- [How to send data from page to a persistent layout](./2023/2023-07-30-how-to-send-data-from-page-to-layout.md)
-
 <ul>
-    <template v-for="article in articles.data">
-      <li>[{{ article.split('/').pop().split('.md')[0] }}]({{ article }})</li>
+    <template v-for="article in articles">
+      <li><a :href="article.url">{{ article.frontmatter.title }}</a></li>
     </template>
 </ul>
 
-
-<pre>{{ articles }}</pre>
